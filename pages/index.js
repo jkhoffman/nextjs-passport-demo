b4b4eca2ac5e2124de2bc954076174fb5ac56157
Jeffery Hoffman <jkhoffman@gmail.com>
2020-07-12 16:14:11 -0400

    Clean out template

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,3 @@
-import Link from "next/link";
-
 export default function IndexPage() {
-  return (
-    <div>
-      Hello World.{" "}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
-  );
+  return <div>Hello World.</div>;
 }
